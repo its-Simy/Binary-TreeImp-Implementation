6a54c930e57bc5ce9840e27f871f971484593143
its-Simy <135281067+its-Simy@users.noreply.github.com>
2024-11-19 10:37:22 -0500

    Added more implementation
    Almost complete, needs to add the delete

--- BST.java
+++ BST.java
@@ -35,4 +35,55 @@ public class BST {
         }
     }
 
+    public void preOrderTraversal(){
+        System.out.println("Pre-order Traversal: ");
+        preOrderRec(root);
+        System.out.println();
+    }
+
+    private void preOrderRec(Node root){
+        if(root != null){
+            //VLR , Visit -> Left -> Right
+            System.out.println(root.getData() + " ");
+            preOrderRec(root.getLeft());
+            preOrderRec(root.getRight());
+        }
+    }
+
+    //Post-Order traversal of the BST
+    public void postOrderTraversal(){
+        System.out.println("Post-order Traversal: ");
+        postOrderRec(root);
+        System.out.println();
+    }
+
+    private void postOrderRec(Node root){
+        //LVR Left -> Right -> Visit
+        if(root != null){
+            postOrderRec(root.getLeft());
+            postOrderRec(root.getRight());
+            System.out.println(root.getData() + " ");
+        }
+    }
+
+    //Search for a value in the BST
+    public boolean search(int data){return searchRec(root,data);}
+
+    private boolean searchRec(Node root,int data) {
+        if (root == null) {
+            return false;
+        }
+        if (root.getData() == data) {
+            return true;
+        }
+        //Inline if statement, whatever is before the ? is the actual statement for the if statement
+        // : indicates what is done for else statement
+        return data < root.getData() ? searchRec(root.getLeft(), data): searchRec(root.getRight(), data);
+    }
+
+    //delete a node from the BST
+    public void delete(int data){root = deletRec(root,data);}
+
+
+
 }
